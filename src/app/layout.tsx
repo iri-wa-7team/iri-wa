ea159492d6ce525c476d3f4f8993d6e30e2d7cc7
deswaq1220 <deswaq789@naver.com>
2024-11-29 19:27:34 +0900

    🎉

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.css';
+
+const pretendard = localFont({
+  src: './fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+  variable: '--font-pretendard',
+});
+
+export const metadata: Metadata = {
+  title: '와이리',
+  description: '여행 인플루언서, 체험단 마케팅 무상 지원 플랫폼',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={`${pretendard.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
